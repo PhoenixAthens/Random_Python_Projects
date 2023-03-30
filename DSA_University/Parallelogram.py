58b3717cf38863981f05279bc2e5064e02aa14db
Phoenix <anmolkhanna2003@gmail.com>
2023-03-30 15:10:52 +0530

    University_DSA: Inheritance Demonstration

---
+++ DSA_University/Parallelogram.py
@@ -0,0 +1,36 @@
+import math
+class Parallelogram:
+    def __init__(self, length:int, breadth:int):
+        self.side1:int=length
+        self.side2:int=breadth
+
+    def Perimeter(self):
+        return 2* ( self.side2+self.side1 )
+    def Area(self):
+        return self.side2*self.side1
+
+class Rectangle(Parallelogram):
+    def __init__(self, length, breadth):
+        super().__init__(length,breadth)
+
+class Square(Rectangle):
+    def __init__(self, side):
+        super().__init__(side,side)
+
+class RightTriangle(Rectangle):
+    def __init__(self, base, height):
+        super().__init__(base, height)
+
+    def Area(self):
+        return 1/2*super().Area()
+
+    def calcHypotenuse(self):
+        return math.sqrt(math.pow(self.side1,2)+math.pow(self.side2,2))
+
+    def Perimeter(self):
+        return self.side1+self.side2+self.calcHypotenuse()
+
+triangle1= RightTriangle(3,4)
+print(triangle1.Area())
+print(triangle1.calcHypotenuse())
+print(triangle1.Perimeter())
